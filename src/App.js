eaf48fab31c664ae7f1ee8f19316ef2c98fe7676
raheebgill29 <raheebuser@gmail.com>
2023-12-24 17:04:39 +0500

    changes

---
+++ src/App.js
@@ -0,0 +1,38 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"> 
+        <div className="lavender-flower">
+          🌸🌸🌸
+        </div>
+        <div className="teddy-bear">
+          🧸
+        </div>
+        <div className="message">
+
+
+          <h2>You Are One OF My Best Friends</h2>
+          
+          <p>Dear Amina,</p>
+          <p>
+            As we get ready to step into a new year, I can't help but think about all the times we've shared. You've been with me through thick and thin, like a true friend. Pablo Neruda once wrote about finding beauty in the ordinary, and that's exactly what our friendship is – extraordinary in its simplicity. From everyday moments to the special ones, each laugh, tear, and secret shared has added a new chapter to our story.
+          </p>
+          <p>
+            T.S. Eliot wondered about time and life, and looking back, I cherish the moments we've created together. As we approach a new year, I wish we meet a lot this year and make more memories.
+          </p>
+          <p>
+            Here's to more adventures, more laughs, and more memories in the upcoming year. I'm grateful for our friendship and excited to see what the future holds.
+          </p>
+          <p>Wishing you a fantastic New Year!</p>
+          <p className="signature">With love, Raheeb Gill</p>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
